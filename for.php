bf3ae53edc03e31c38a3dd194a3d173ba40e5b7e
itonttu <42998227+itonttu@users.noreply.github.com>
2023-03-09 10:32:39 +0900

    1st commit

---
+++ for.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do while</title>
+</head>
+<body>
+<?php
+    $num = 10;
+    echo $num,'個の●を表示します。<br>';
+    for ( $cnt = 0; $cnt < $num; $cnt++){
+        echo'●';
+    }
+?>
+</body>
+</html>
